7577f27cac2cce0adf1aeb6bcb34f5b56c04fe59
Tony Afanasev <antonsacred@gmail.com>
2026-02-28 08:22:35 +0100

    Fix test by changing the log level

--- tests/Config/SentryMonologConfigTest.php
+++ tests/Config/SentryMonologConfigTest.php
@@ -38,7 +38,7 @@ final class SentryMonologConfigTest extends TestCase
             $prodConfig['services']['Sentry\\Monolog\\Handler']['arguments']['$hub'] ?? null,
         );
         self::assertSame(
-            Logger::WARNING,
+            Logger::ERROR,
             $prodConfig['services']['Sentry\\Monolog\\Handler']['arguments']['$level'] ?? null,
         );
         self::assertSame(
